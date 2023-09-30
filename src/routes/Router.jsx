80f53fa7a574052c3bf980fe83615ac736d20051
naimuddin94 <developernaimuddin@gmail.com>
2023-09-30 18:59:02 +0600

    Home page add

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,12 +4,17 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Cart from "../pages/Cart/Cart";
 import PrivateRoute from "./PrivateRoute";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "/register",
         element: <Register />,
